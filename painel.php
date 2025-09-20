ce23d048aabcd72308f36705ab7411d45571a6ac
Andrey Barfkencht Rodrigues <andrey30r@gmail.com>
2025-09-19 22:54:06 -0400

    novo painel

--- painel.php
+++ painel.php
@@ -3,13 +3,19 @@
 require 'templates/header.php'; 
 ?>
 
-<h2>Bem-vindo ao Painel de Controle!</h2>
-<p>
-    Olá, <strong><?php echo htmlspecialchars($_SESSION['usuario_nome']); ?></strong>.
-</p>
-<p>
-    Utilize o menu acima para navegar entre as funcionalidades do sistema, como cadastrar novos alunos, responsáveis e turmas.
-</p>
+<main class="painel-container">
+
+    <aside class="painel-lateral">
+        <h2>Bem-vindo ao Painel de Controle!</h2>
+        Olá, <strong><?php echo htmlspecialchars($_SESSION['usuario_nome']); ?></strong>.
+        <p>Utilize o menu acima para navegar entre as funcionalidades do sistema, como cadastrar novos alunos, responsáveis e turmas.</p>
+    </aside>
+
+    <section class="conteudo-principal">
+        <p>Selecione uma opção no menu acima para começar.</p>
+    </section>
+
+</main>
 
 <?php 
 // Inclui o rodapé
